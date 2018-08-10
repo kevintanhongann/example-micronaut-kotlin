fd41108c75951b1bae829748bf5413029943db13
kevintan <kevintanhongann@gmail.com>
2018-08-10 13:12:25 +0800

    test hellocontroller

--- src/main/kotlin/example/micronaut/kotlin/HelloController.kt
+++ src/main/kotlin/example/micronaut/kotlin/HelloController.kt
@@ -1,9 +1,11 @@
 package example.micronaut.kotlin
 
 import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
 
+@Controller("/hello")
 class HelloController {
   @Get("/")
   @Produces(MediaType.TEXT_PLAIN)
---
+++ src/test/kotlin/example/micronaut/kotlin/HelloControllerSpec.kt
@@ -0,0 +1,24 @@
+package example.micronaut.kotlin
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.client.HttpClient
+import io.micronaut.runtime.server.EmbeddedServer
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.on
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class HomeControllerSpec: Spek({
+  describe("HelloController") {
+    var embeddedServer : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java)
+    var client : HttpClient = HttpClient.create(embeddedServer.url)
+    on("test /hello responds Hello World") {
+      var rsp : String = client.toBlocking().retrieve("/hello")
+      assertEquals(rsp, "Hello World")
+    }
+    afterGroup {
+      client.close()
+      embeddedServer.close()
+    }
+  }
+})
